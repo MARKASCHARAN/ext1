3505a4d99d47a5458f66739e97f25e28d7728703
MARKA SAI CHARAN <133427225+MARKASCHARAN@users.noreply.github.com>
2024-09-03 21:36:08 +0530

    Add files via upload

---
+++ ext1/popup.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const macButton = document.getElementById('mac-button');
+  const windowsButton = document.getElementById('windows-button');
+  const linuxButton = document.getElementById('linux-button');
+
+  const macContent = document.getElementById('mac-content');
+  const windowsContent = document.getElementById('windows-content');
+  const linuxContent = document.getElementById('linux-content');
+
+  
+  function hideAllContent() {
+    macContent.classList.remove('active');
+    windowsContent.classList.remove('active');
+    linuxContent.classList.remove('active');
+  }
+
+  macButton.addEventListener('click', function() {
+    hideAllContent();
+    macContent.classList.add('active');
+    
+  });
+
+  windowsButton.addEventListener('click', function() {
+    hideAllContent();
+    windowsContent.classList.add('active');
+    
+  });
+
+  linuxButton.addEventListener('click', function() {
+    hideAllContent();
+    linuxContent.classList.add('active');
+    
+  });
+});
